4c494ffe41ab89fee9bfa7c658ccae1dd236c10e
TyomoGit <riku3dno.1@gmail.com>
2023-09-19 16:38:26 +0900

    pkg

---
+++ pkg/brainfuck_wasm.d.ts
@@ -0,0 +1,52 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+* @param {string} string
+* @returns {InterpreterContext}
+*/
+export function new_interpreter(string: string): InterpreterContext;
+/**
+*/
+export class InterpreterContext {
+  free(): void;
+/**
+*/
+  step(): void;
+/**
+*/
+  run(): void;
+}
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+  readonly memory: WebAssembly.Memory;
+  readonly __wbg_interpretercontext_free: (a: number) => void;
+  readonly interpretercontext_step: (a: number) => void;
+  readonly interpretercontext_run: (a: number) => void;
+  readonly new_interpreter: (a: number, b: number) => number;
+  readonly __wbindgen_malloc: (a: number, b: number) => number;
+  readonly __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+  readonly __wbindgen_exn_store: (a: number) => void;
+}
+
+export type SyncInitInput = BufferSource | WebAssembly.Module;
+/**
+* Instantiates the given `module`, which can either be bytes or
+* a precompiled `WebAssembly.Module`.
+*
+* @param {SyncInitInput} module
+*
+* @returns {InitOutput}
+*/
+export function initSync(module: SyncInitInput): InitOutput;
+
+/**
+* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+* for everything else, calls `WebAssembly.instantiate` directly.
+*
+* @param {InitInput | Promise<InitInput>} module_or_path
+*
+* @returns {Promise<InitOutput>}
+*/
+export default function __wbg_init (module_or_path?: InitInput | Promise<InitInput>): Promise<InitOutput>;
---
+++ pkg/brainfuck_wasm_bg.wasm.d.ts
@@ -0,0 +1,10 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function __wbg_interpretercontext_free(a: number): void;
+export function interpretercontext_step(a: number): void;
+export function interpretercontext_run(a: number): void;
+export function new_interpreter(a: number, b: number): number;
+export function __wbindgen_malloc(a: number, b: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number, d: number): number;
+export function __wbindgen_exn_store(a: number): void;

edefad728afe101a40f1c1aacee0f23553411359
TyomoGit <riku3dno.1@gmail.com>
2023-09-20 12:18:43 +0900

    design

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ import init, * as wasm from "./pkg/brainfuck_wasm.js";
 const code = document.getElementById('code');
 const runButton = document.getElementById("runButton");
 const output = document.getElementById("output");
+
+const buttons = document.querySelectorAll("button");
 let interpreter;
 
 init().then(() => {
@@ -11,6 +13,19 @@ init().then(() => {
 
 function main() {
     runButton?.addEventListener("click", onButtonClick);
+    buttons.forEach(button => {
+        if (button.parentElement.id != "keys") {
+            return
+        }
+
+        button.addEventListener("click", () => {
+            if (button.innerText == "↩️") {
+                code.innerHTML += "\n";
+            } else {
+                code.innerHTML += button.innerText;
+            }
+        });
+    });
 }
 
 function onButtonClick() {
